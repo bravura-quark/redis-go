cd584bea995c44e4934848b653962ebdec9f659d
Vamsi Krishna <discover.with.vamsi@gmail.com>
2022-12-02 21:57:02 +0530

    implement the echo command

--- app/server.go
+++ app/server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"net"
 	"os"
 )
@@ -30,19 +30,21 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	for {
-		buf := make([]byte, 1024)
-
-		if _, err := conn.Read(buf); err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				fmt.Println("error reading from client: ", err.Error())
-				os.Exit(1)
-			}
+		value, err := DecodeRESP(bufio.NewReader(conn))
+		if err != nil {
+			fmt.Println("Error decoding RESP: ", err.Error())
 		}
 
-		// Let's ignore the client's input for now and hardcode a response.
-		// We'll implement a proper Redis Protocol parser in later stages.
-		conn.Write([]byte("+PONG\r\n"))
+		command := value.Array()[0].String()
+		args := value.Array()[1:]
+
+		switch command {
+		case "ping":
+			conn.Write([]byte("+PONG\r\n"))
+		case "echo":
+			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[0].String()), args[0].String())))
+		default:
+			conn.Write([]byte("-ERR unknown command '" + command + "'\r\n"))
+		}
 	}
 }
